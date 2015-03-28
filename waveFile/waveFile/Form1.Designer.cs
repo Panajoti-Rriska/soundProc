e3859dadcb8a6aa67d85c5a0da0e123307577c84
Panajoti-Rriska <panoslennon@gmail.com>
2015-03-28 17:03:30 +0100

    init commit

---
+++ waveFile/waveFile/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace waveFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openWavButton = new System.Windows.Forms.Button();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openWavButton
+            // 
+            this.openWavButton.Location = new System.Drawing.Point(68, 12);
+            this.openWavButton.Name = "openWavButton";
+            this.openWavButton.Size = new System.Drawing.Size(159, 45);
+            this.openWavButton.TabIndex = 0;
+            this.openWavButton.Text = "OpenWavFile";
+            this.openWavButton.UseVisualStyleBackColor = true;
+            this.openWavButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.Location = new System.Drawing.Point(68, 87);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(159, 44);
+            this.pauseButton.TabIndex = 1;
+            this.pauseButton.Text = "Play/Pause Wav";
+            this.pauseButton.UseVisualStyleBackColor = true;
+            this.pauseButton.Click += new System.EventHandler(this.pauseButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 205);
+            this.Controls.Add(this.pauseButton);
+            this.Controls.Add(this.openWavButton);
+            this.Name = "Form1";
+            this.Text = "WavPlayer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openWavButton;
+        private System.Windows.Forms.Button pauseButton;
+    }
+}
+
