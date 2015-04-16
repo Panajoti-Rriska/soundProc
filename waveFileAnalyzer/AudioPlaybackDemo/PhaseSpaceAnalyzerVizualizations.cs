92d7f9a27776f5cca6aba59c0de64dfe2f1009fc
Panajoti-Rriska <panoslennon@gmail.com>
2015-04-17 01:52:23 +0200

    phase space analyzer, not working one

---
+++ waveFileAnalyzer/AudioPlaybackDemo/PhaseSpaceAnalyzerVizualizations.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.Composition;
+
+namespace NAudioWpfDemo.AudioPlaybackDemo
+{
+     [Export(typeof(IVisualizationPlugin))]
+    class PhaseSpaceAnalyzerVizualizations : IVisualizationPlugin
+    {
+         private PhaseSpace phaseSpaceAnalyzer = new PhaseSpace();
+
+         public string Name
+         {
+             get { return "Phase Space Analyser"; }
+         }
+
+         public object Content
+         {
+             get { return phaseSpaceAnalyzer; }
+         }
+
+         public void OnMaxCalculated(float min, float max)
+         {
+             // nothing to do
+         }
+
+         public void OnFftCalculated(NAudio.Dsp.Complex[] result)
+         {
+             //spectrumAnalyser.Update(result);
+         }
+    }
+}
